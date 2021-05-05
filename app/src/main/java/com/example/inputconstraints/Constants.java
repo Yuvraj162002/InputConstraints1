4d8dd877403b55479f24c04676589663bcb8b70c
Yuvraj@162002 <yuvrajpareek44@gmail.com>
2021-05-05 14:10:57 +0530

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/inputconstraints/Constants.java
@@ -0,0 +1,10 @@
+package com.example.inputconstraints;
+
+public class Constants {
+    public static final String UPPERCASE_KEY = "uupercase";
+    public static final String LOWERCASE_KEY = "lowecase";
+    public static final String DIGITS_KEY = "Digits";
+    public static final String EXPRESSION_KEY = "expression";
+    public static final String SYMBOL_KEY = "symbol";
+    public static final String CONSTRAINT_KEY = "constraint";
+}
